23f1a71122864dcfd1b460bb78e55999b0c9278b
Sheyla <Sheylapopovich@gmail.com>
2022-03-11 10:00:19 -0700

    added an event listern to play varible to play sound on app

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 const app = () => {
-    const song = document.querySelector('.song');
-    const play = document.querySelector('.play');
-    const outline = document.querySelector('.moving-outline circle');
-    const video = document.querySelector('.vid-container video');
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
 
-    //App Sounds
-    const sounds = document.querySelectorAll('.sound-picker button');
+  //App Sounds
+  const sounds = document.querySelectorAll(".sound-picker button");
 
-    //Time display
-    const timeDisplay =document.querySelector('.time-display');
+  //Time display
+  const timeDisplay = document.querySelector(".time-display");
 
-    //Get the length of the outline
-    const outlineLength = outline.getTotalLength();
-    console.log(outlineLength);
+  //Get the length of the outline
+  const outlineLength = outline.getTotalLength();
+  console.log(outlineLength);
 
-    //Duration 
-    let fakeDuration = 600;
+  //Duration
+  let fakeDuration = 600;
 
-    outline.style.strokeDasharray = outlineLength;
-    outline.style.strokeDashoffset = 500;
-}
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  //Play sound
+  play.addEventListener("click", () => {
+    song.play();
+  });
+};
 app();
